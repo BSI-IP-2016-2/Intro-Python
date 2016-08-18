c7bbfeb451098c04f0be44e5cbbd3b3ee767cdf5
Roxak <rodrigo.xcarvalho@ufrpe.br>
2016-08-18 20:42:13 -0300

    Código de Cálculo da Base do Retângulo

---
+++ CalcRet.py
@@ -0,0 +1,15 @@
+def calcRet():
+    x = input("Digite a base: ")
+    while not x:
+        x = input("Digite a base: ")
+    y = input("Digite a altura: ")
+    while not y:
+        y = input("Digite a altura: ")
+    if int(x) == 0 or int(y) == 0:
+        print("O valor digitado está incorreto. Por favor, tente novamente: ")
+    elif int(x) > 0 and int(y) > 0:
+        print(int(x) * int(y))
+    return calcRet()
+
+if __name__ == '__main__':
+    calcRet()
