946b454efb27068ab57c539ad358ec98c48b44b2
Roxak <rodrigo.xcarvalho@ufrpe.br>
2016-08-18 23:35:24 -0300

    Update VerificarMatricula.py

--- VerificarMatricula.py
+++ VerificarMatricula.py
@@ -1,11 +1,11 @@
-Nome = str(input("Digite o nome do aluno: "))
-Matr = 0
-while True:
+Nome = str(input("Digite o nome do aluno: ")) #Mostra na tela o input para o nome do aluno ser digitado
+Matr = 0 #Define a variável inicial da matrícula
+while True: #Entra no loop para verificar se a matrícula está sendo digitadas em números inteiros
 	try:
 		Matr = int(input("Digite o número da matrícula do aluno: "))
 	except ValueError:
 		print("O número de matrícula deve conter apenas números")
-		continue
+		continue #Senão for, entra no loop e pede novamente para digitar a matrícula
 	else:
-		break
-print ("Nome do Aluno: " + Nome + ",", "Matricula: %s" %(Matr))
+		break #Fecha o loop caso esteja tudo certo
+print ("Nome do Aluno: " + Nome + ",", "Matricula: %s" %(Matr)) #Exibe o nome do aluno e a matrícula na formatação exigida
