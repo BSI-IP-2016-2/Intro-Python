ad5ab9c1920d36661a5529a48d10c3276e3de0a7
Roxak <rodrigo.xcarvalho@ufrpe.br>
2016-08-18 21:21:03 -0300

    Variação do Código de Cálculo do Retângulo

---
+++ CalcRet_VarCod.py
@@ -0,0 +1,25 @@
+def calcRet():
+    x, y = (0,)*2
+    while True:
+        try:
+            x = int(input("Digite a base: "))
+        except ValueError:
+            print("Você não pode deixar o espaço vazio ou digitou uma letra")
+            continue
+        else:
+            break
+    while True:
+        try:
+            y = int(input("Digite a altura: "))
+        except ValueError:
+            print("Você não pode deixar o espaço vazio ou digitou uma letra")
+            continue
+        else:
+            break
+    if int(x) > 0 and int(y) > 0:
+        print(int(x) * int(y))
+        return ()
+
+if __name__ == '__main__':
+    calcRet()
+
