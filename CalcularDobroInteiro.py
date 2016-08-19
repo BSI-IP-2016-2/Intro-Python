a5b418c3475045037011ea81ab47d6135eb1445d
Roxak <rodrigo.xcarvalho@ufrpe.br>
2016-08-18 23:27:03 -0300

    Update CalcularDobroInteiro.py

--- CalcularDobroInteiro.py
+++ CalcularDobroInteiro.py
@@ -1,10 +1,10 @@
-x = 0
-while True:
-    try:
-        x = float(input("Digite um número inteiro: "))
-    except ValueError:
+x = 0 #Define a variável x inicial
+while True: #Entra no loop
+    try: 
+        x = float(input("Digite um número inteiro: ")) #Verificar se o número digitado é um float (inteiro ou decimal)
+    except ValueError: #Senão for, joga um erro e uma mensagem pro usuário
         print("Por favor, digite apenas números inteiros")
         continue
     else:
-        break
-print(x * 2)
+        break #Fecha o loop quando recebe a confirmação que o número foi digitado
+print(x * 2) #Cálcula o resultado
