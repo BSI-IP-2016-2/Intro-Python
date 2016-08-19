75e66613b7c14f1111b3a536975dbd0824f17324
Roxak <rodrigo.xcarvalho@ufrpe.br>
2016-08-18 23:30:57 -0300

    Update CalcularImposto.py

--- CalcularImposto.py
+++ CalcularImposto.py
@@ -1,11 +1,11 @@
-xCheque = 0
+xCheque = 0 #Define a variável que representa o cheque que terá o imposto aplicado
 while True:
     try:
-        xCheque = float(input("Digite o salário: "))
+        xCheque = float(input("Digite o salário: ")) #Espera para receber o valor do cheque e identificar se ele é float (inteiro ou decimal)
     except ValueError:
         print("Você não pode deixar o espaço vazio ou digitou uma letra")
-        continue
+        continue #Se tiver tudo certo ele continua, se não volta pro "try"
     else:
-        break
-Tax = xCheque * 0.3
-print('O salário final será: %d' % (Tax))
+        break #Fecha o loop
+Tax = xCheque * 0.3 #Faz o cálculo do imposto e armazena o valor na variável "Tax"
+print('O salário final será: %d' % (Tax)) #Mostra a mensagem com a taxa que foi descontada
