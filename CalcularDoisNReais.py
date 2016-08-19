88b69b85a3b4ab9de7e3ac9344c1990bd35c9580
Roxak <rodrigo.xcarvalho@ufrpe.br>
2016-08-18 23:29:08 -0300

    Update CalcularDoisNReais.py

--- CalcularDoisNReais.py
+++ CalcularDoisNReais.py
@@ -1,13 +1,13 @@
-x, y = (0,)*2
+x, y = (0,)*2 #Define as duas variáveis iniciais que serão usadas
 while True:
     try:
-        x = float(input("Digite um número real: "))
+        x = float(input("Digite um número real: ")) #Verifica se o número digitado é um float (inteiro ou decimal)
     except ValueError:
-        print("Por favor, digite apenas números inteiros")
+        print("Por favor, digite apenas números inteiros") #Joga a mensagem de erro pro usuário saber o que ocorreu e digitar de novo
         continue
     else:
-        break
-while True:
+        break #Fecha o loop
+while True: #Abre um novo loop para esperar o segundo número
     try:
         y = float(input("Digite um segundo número real: "))
     except ValueError:
@@ -15,4 +15,4 @@ while True:
         continue
     else:
         break
-print(x * y)
+print(x * y) #Faz o cálculo final e mostra pro usuário
