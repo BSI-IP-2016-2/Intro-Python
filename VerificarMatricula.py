058a405fb9b59997a27223f4845fb2185c961828
Rodrigo Xavier <rodrigo.xcarvalho@ufrpe.br>
2016-08-19 09:27:21 -0300

    Update VerificarMatricula.py

--- VerificarMatricula.py
+++ VerificarMatricula.py
@@ -1,3 +1,5 @@
+# Forma em Python 2.x para exibir a matricula do aluno no python atualizado:
+
 Nome = str(input("Digite o nome do aluno: ")) #Mostra na tela o input para o nome do aluno ser digitado
 Matr = 0 #Define a variável inicial da matrícula
 while True: #Entra no loop para verificar se a matrícula está sendo digitadas em números inteiros
@@ -11,9 +13,9 @@ while True: #Entra no loop para verificar se a matrícula está sendo digitadas 
 print ("Nome do Aluno: " + Nome + ",", "Matricula: %s" %(Matr)) #Exibe o nome do aluno e a matrícula na formatação exigida
 
 
-# Forma para exibir a matricula do aluno no python atualizado:
+# Forma em Python 2.x para exibir a matricula do aluno no python atualizado:
 
-    nome = raw_input('Escreva o nome do aluno:') # Mostra na tela para digitar o nome do aluno
+nome = raw_input('Escreva o nome do aluno:') # Mostra na tela para digitar o nome do aluno
 matricula = 0 #define a variável inicial da matrícula
 while True: #setando o loop
     try:
