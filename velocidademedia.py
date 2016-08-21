c3b4c2f458c167542a470f1247222c1c407bbc87
Brigido <lucas.brigido@ufrpe.br>
2016-08-20 21:55:28 -0300

    Update velocidademedia.py

--- velocidademedia.py
+++ velocidademedia.py
@@ -1,31 +1,33 @@
-'''
-Created on 19 de ago de 2016
+# Cálculo da Velocidade Média, usando tanto minutos quanto horas.
 
-@author: NettHacker
-'''
-
-if __name__ == '__main__':
-    print('Exercicio da velocidade media')
-vel1 = 0
-vel2 = 0
+    print('Exercício de Velocidade Média')
+dist = 0 # Defina as variáveis
 temp = 0
-while True:
+x = 0
+y = 0
+while True: # abrir o loop, para evitar que se ponha letras em vez de números
     try:
-        dist = float(input('Digite a distancia percorrida: '))
-        x = float(input('Se o tempo for em horas digite 1 se em minutos 2: '))
-        temp = float(input('Se usar horas digite o tempo em horas, se minutos em minutos: '))
+        dist = float(input('Digite a distância percorrida: '))
+        temp = float(input('Digite o tempo gasto em horas ou em minutos: '))
+        x = int(input('Digite 1 se utilizou horas ou 2 se utilizou minutos: '))
     except ValueError:
-            print('Somente numeros, por favor!')
-            continue
+        print('Somente números !')
+        continue
     else:
         break
 
-if x == 1 :
-    vel1 = dist/temp
-else:
-    vel2 = dist*60/temp
+#Definindo se x == 1, 2 ou se nenhum dos 2
 
-if vel1 > 110 or vel2 > 0 :
-    print('Sua velocidade media sera acima de 110km/h, que sera de: %dkm/h'%(vel1 or vel2))
+if x == 1:
+    y = dist/temp
+elif x == 2:
+    y = dist*60/temp
 else:
-    print('Sua velocidade media sera de: %s'%(vel1 or vel2))
+    print('Erro, Digite 1 para horas ou 2 para minutos!')  # Caso não selecione 1 ou 2, o cálculo vai dar 0 e vai dar Erro  
+
+#o próximo if está relacionado se o y for maior que 110 ou não !
+
+if y > 110 :
+    print('Velocidade média acima de 110km/h, sua velocidade foi de : %s km/h'%(y))
+else:
+    print('Sua velocidade foi de: %s km/h'%(y))
